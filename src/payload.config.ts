196cc7ca426a4acf4646fd0ed9e2e30673862713
geofrey254 <devongeoffreymaina@gmail.com>
2026-03-10 21:25:08 +0300

    Added payload public url to server

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -31,8 +31,8 @@ export default buildConfig({
   },
   collections: [Users, Media, Blog, Pages, Services, ModularServices, Team, Newsletter],
   editor: lexicalEditor(),
-  // serverURL: process.env.NEXT_PUBLIC_PAYLOAD_URL,
-  serverURL: 'http://localhost:3000',
+  serverURL: process.env.NEXT_PUBLIC_PAYLOAD_URL,
+  // serverURL: 'http://localhost:3000',
   secret: process.env.PAYLOAD_SECRET || '',
   typescript: {
     outputFile: path.resolve(dirname, 'payload-types.ts'),
