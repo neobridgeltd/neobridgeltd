00348cafa1ada37f155d1c48a28507d0bdc8a2a7
geofrey254 <devongeoffreymaina@gmail.com>
2026-03-11 16:23:31 +0300

    added neobridge to image domains

--- next.config.mjs
+++ next.config.mjs
@@ -19,6 +19,10 @@ const nextConfig = {
       {
         protocol:'https',
         hostname:'neobridgeltd-gilt.vercel.app'
+      },
+      {
+        protocol:'https',
+        hostname:'neobridgeltd.co.ke'
       }
     
     ]
